adaa473ae1a785aa628bb4abf0d636a1c6b86d55
Hector Huertas <hectorhuertas@gmail.com>
2016-04-18 17:15:46 +0200

    Implement links sort

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $('#all').on('click', all);
   $('#read').on('click', read);
   $('#unread').on('click', unread);
+  $('#sort').on('click', sort);
 });
 
 function searchFilter (e) {
@@ -36,4 +37,18 @@ function all (e) {
   $('#links li').each(function (index, li) {
       $(li).show();
   });
+}
+
+function sort (e) {
+  var sorted = $('#links li').sort(sortFunction);
+  $('#links').empty().append(sorted);
+}
+
+function sortFunction (a,b) {
+  var keyA = $(a).find('span').text();
+  var keyB = $(b).find('span').text();
+
+  if (keyA < keyB) return -1;
+  if (keyA > keyB) return 1;
+  return 0;
 }
