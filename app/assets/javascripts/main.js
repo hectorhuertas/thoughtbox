f7e7fe1284ebc0e01d5d13b3868f3b68ff6b9d16
Hector Huertas <hectorhuertas@gmail.com>
2016-04-18 16:47:24 +0200

    Implement status filter

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,39 @@
 $(document).ready(function(){
   $('#search-filter').on('keyup', searchFilter);
+  $('#all').on('click', all);
+  $('#read').on('click', read);
+  $('#unread').on('click', unread);
 });
 
 function searchFilter (e) {
   var search = e.target.value;
-  console.log('bob');
   $('#links li').each(function (index, li) {
     if (li.textContent.indexOf(search) >= 0) {
       $(li).show();
     } else {
       $(li).hide();
     }
+  });
+}
+
+function read (e) {
+  $('#links li').each(function (index, li) {
+    if (li.textContent.indexOf('Unread') >= 0) {
+      $(li).hide();
+    }
+  });
+}
+
+function unread (e) {
+  $('#links li').each(function (index, li) {
+    if (li.textContent.indexOf('Unread') < 0) {
+      $(li).hide();
+    }
+  });
+}
+
+function all (e) {
+  $('#links li').each(function (index, li) {
+      $(li).show();
   });
 }
