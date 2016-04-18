a0b9b56b52e313d48f1083e70ac9f3efd67cc9b5
Hector Huertas <hectorhuertas@gmail.com>
2016-04-18 12:40:27 +0200

    Implement link submission

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,20 @@
 class LinksController < ApplicationController
   def index
+    @link  = Link.new
+    @links = Link.all
   end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :index
+    end
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 end
